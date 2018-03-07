7f92e5b2b6916c58f373491b12eca1a0f6cbee21
danilkuzin <danilkuzin@gmail.com>
2018-03-07 23:53:28 +0000

    collect results on synthetic and script for synthetic active learning

--- theano/experiments/synthetic/number_of_layers/collect_results.py
+++ theano/experiments/synthetic/number_of_layers/collect_results.py
@@ -10,13 +10,17 @@ class NumberOfLayersExperimentReultsCollector(object):
         self.n_iter = 1000
 
     def collect_all(self):
-        self.ista_f_meas_train, self.ista_f_meas_validation, self.ista_loss_train, self.ista_loss_validation = \
+        self.ista_f_meas_train, self.ista_f_meas_validation, self.ista_loss_train, self.ista_loss_validation, \
+        self.ista_f_meas_train_std, self.ista_f_meas_validation_std, self.ista_loss_train_std, self.ista_loss_validation_std = \
             self.collect_ista_fista('ista')
-        self.fista_f_meas_train, self.fista_f_meas_validation, self.fista_loss_train, self.fista_loss_validation = \
+        self.fista_f_meas_train, self.fista_f_meas_validation, self.fista_loss_train, self.fista_loss_validation, \
+        self.fista_f_meas_train_std, self.fista_f_meas_validation_std, self.fista_loss_train_std, self.fista_loss_validation_std, = \
             self.collect_ista_fista('fista')
-        self.freq_f_meas_train, self.freq_f_meas_validation, self.freq_loss_train, self.freq_loss_validation = \
+        self.freq_f_meas_train, self.freq_f_meas_validation, self.freq_loss_train, self.freq_loss_validation, \
+        self.freq_f_meas_train_std, self.freq_f_meas_validation_std, self.freq_loss_train_std, self.freq_loss_validation_std = \
             self.collect_main('freq')
-        self.sh_bayes_f_meas_train, self.sh_bayes_f_meas_validation, self.sh_bayes_loss_train, self.sh_bayes_loss_validation = \
+        self.sh_bayes_f_meas_train, self.sh_bayes_f_meas_validation, self.sh_bayes_loss_train, self.sh_bayes_loss_validation,\
+        self.sh_bayes_f_meas_train_std, self.sh_bayes_f_meas_validation_std, self.sh_bayes_loss_train_std, self.sh_bayes_loss_validation_std = \
             self.collect_main('sh_bayes')
 
     def collect_ista_fista(self, filename):
@@ -33,6 +37,16 @@ class NumberOfLayersExperimentReultsCollector(object):
                 loss_train[rseed, i] = saved_res['train_loss']
                 loss_validation[rseed, i] = saved_res['validation_loss']
 
+        f_meas_train_std = np.std(f_meas_train, axis=0)
+        f_meas_validation_std = np.std(f_meas_train, axis=0)
+        loss_train_std = np.std(f_meas_train, axis=0)
+        loss_validation_std = np.std(f_meas_train, axis=0)
+
+        f_meas_train_std = f_meas_train_std[:, -1]
+        f_meas_validation_std = f_meas_validation_std[:, -1]
+        loss_train_std = loss_train_std[:, -1]
+        loss_validation_std = loss_validation_std[:, -1]
+
         f_meas_train = np.mean(f_meas_train, axis=0)
         f_meas_validation = np.mean(f_meas_validation, axis=0)
         loss_train = np.mean(loss_train, axis=0)
@@ -43,7 +57,8 @@ class NumberOfLayersExperimentReultsCollector(object):
         loss_train = loss_train[:, -1]
         loss_validation = loss_validation[:, -1]
 
-        return f_meas_train, f_meas_validation, loss_train, loss_validation
+        return f_meas_train, f_meas_validation, loss_train, loss_validation, \
+               f_meas_train_std, f_meas_validation_std, loss_train_std, loss_validation_std
 
     def collect_main(self, alg_name):
         f_meas_train = np.zeros((self.random_range, len(self.L_array), self.n_iter))
@@ -58,6 +73,16 @@ class NumberOfLayersExperimentReultsCollector(object):
             f_meas_train[i] = saved_res['{}_train_f_measure'.format(alg_name)]
             f_meas_validation[i] = saved_res['{}_validation_f_measure'.format(alg_name)]
 
+        f_meas_train_std = np.std(f_meas_train, axis=0)
+        f_meas_validation_std = np.std(f_meas_train, axis=0)
+        loss_train_std = np.std(f_meas_train, axis=0)
+        loss_validation_std = np.std(f_meas_train, axis=0)
+
+        f_meas_train_std = f_meas_train_std[:, -1]
+        f_meas_validation_std = f_meas_validation_std[:, -1]
+        loss_train_std = loss_train_std[:, -1]
+        loss_validation_std = loss_validation_std[:, -1]
+
         f_meas_train = np.mean(f_meas_train, axis=0)
         f_meas_validation = np.mean(f_meas_validation, axis=0)
         loss_train = np.mean(loss_train, axis=0)
@@ -68,7 +93,8 @@ class NumberOfLayersExperimentReultsCollector(object):
         loss_train = loss_train[:, -1]
         loss_validation = loss_validation[:, -1]
 
-        return f_meas_train, f_meas_validation, loss_train, loss_validation
+        return f_meas_train, f_meas_validation, loss_train, loss_validation, \
+               f_meas_train_std, f_meas_validation_std, loss_train_std, loss_validation_std
 
     def plot_all(self):
 
@@ -78,40 +104,80 @@ class NumberOfLayersExperimentReultsCollector(object):
             if e.errno != errno.EEXIST:
                 raise
 
-        plt.plot(self.L_array, self.ista_f_meas_train, label='ista')
-        plt.plot(self.L_array, self.fista_f_meas_train, label='fista')
-        plt.plot(self.L_array, self.freq_f_meas_train, label='freq lista')
-        plt.plot(self.L_array, self.sh_bayes_f_meas_train, label='sh bayes')
+        linewidth = .25
+        markersize = 5.
+        std_multiplier = 2
+
+        ista_marker= 'o'
+        ista_color='blue'
+        ista_label='ista'
+
+        fista_marker= 'v'
+        fista_color='red'
+        fista_label='fista'
+
+        freq_marker= 's'
+        freq_color='black'
+        freq_label='freq lista'
+
+        sh_bayes_marker='d'
+        sh_bayes_color='green'
+        sh_bayes_label='sh bayes'
+
+        plt.plot(self.L_array, self.ista_f_meas_train, label=ista_label, color=ista_color, marker=ista_marker, linewidth=linewidth, markersize=markersize)
+        plt.errorbar(self.L_array, self.ista_f_meas_train, yerr=std_multiplier * self.ista_f_meas_train_std, color=ista_color)
+        plt.plot(self.L_array, self.fista_f_meas_train, label=fista_label, color=fista_color, marker=fista_marker, linewidth=linewidth, markersize=markersize)
+        plt.errorbar(self.L_array, self.fista_f_meas_train, yerr=std_multiplier * self.fista_f_meas_train_std, color=fista_color)
+        plt.plot(self.L_array, self.freq_f_meas_train, label=freq_label, color=freq_color, marker=freq_marker, linewidth=linewidth, markersize=markersize)
+        plt.errorbar(self.L_array, self.freq_f_meas_train, yerr=2 * self.freq_f_meas_train_std, color=freq_color)
+        plt.plot(self.L_array, self.sh_bayes_f_meas_train, label=sh_bayes_label, color=sh_bayes_color, marker=sh_bayes_marker, linewidth=linewidth, markersize=markersize)
+        plt.errorbar(self.L_array, self.sh_bayes_f_meas_train, yerr=2 * self.sh_bayes_f_meas_train_std, color=sh_bayes_color)
         plt.legend()
-        plt.title('F measure train')
         plt.savefig('results/f_measure_train.eps', format='eps')
+        plt.xlabel(r'K')
+        plt.ylabel(r'F measure')
         plt.show()
 
-        plt.plot(self.L_array, self.ista_f_meas_validation, label='ista')
-        plt.plot(self.L_array, self.fista_f_meas_validation, label='fista')
-        plt.plot(self.L_array, self.freq_f_meas_validation, label='freq lista')
-        plt.plot(self.L_array, self.sh_bayes_f_meas_validation, label='sh bayes')
+        plt.plot(self.L_array, self.ista_f_meas_validation, label=ista_label, color=ista_color, marker=ista_marker, linewidth=linewidth, markersize=markersize)
+        plt.errorbar(self.L_array, self.ista_f_meas_validation, yerr=3 * self.ista_f_meas_validation_std, color=ista_color)
+        plt.plot(self.L_array, self.fista_f_meas_validation, label=fista_label, color=fista_color, marker=fista_marker, linewidth=linewidth, markersize=markersize)
+        plt.errorbar(self.L_array, self.fista_f_meas_validation, yerr=3 * self.fista_f_meas_validation_std, color=fista_color)
+        plt.plot(self.L_array, self.freq_f_meas_validation, label=freq_label, color=freq_color, marker=freq_marker, linewidth=linewidth, markersize=markersize)
+        plt.errorbar(self.L_array, self.freq_f_meas_validation, yerr=3 * self.freq_f_meas_validation_std, color=freq_color)
+        plt.plot(self.L_array, self.sh_bayes_f_meas_validation, label=sh_bayes_label, color=sh_bayes_color, marker=sh_bayes_marker, linewidth=linewidth, markersize=markersize)
+        plt.errorbar(self.L_array, self.sh_bayes_f_meas_validation, yerr=3 * self.sh_bayes_f_meas_validation_std, color=sh_bayes_color)
         plt.legend()
-        plt.title('F measure validation')
         plt.savefig('results/f_measure_validation.eps', format='eps')
+        plt.xlabel(r'K')
+        plt.ylabel(r'F measure')
         plt.show()
 
-        plt.plot(self.L_array, self.ista_loss_train, label='ista')
-        plt.plot(self.L_array, self.fista_loss_train, label='fista')
-        plt.plot(self.L_array, self.freq_loss_train, label='freq lista')
-        plt.plot(self.L_array, self.sh_bayes_loss_train, label='sh bayes')
+        plt.plot(self.L_array, self.ista_loss_train, label=ista_label, color=ista_color, marker=ista_marker, linewidth=linewidth, markersize=markersize)
+        plt.errorbar(self.L_array, self.ista_loss_train, yerr=3 * self.ista_loss_train_std, color=ista_color)
+        plt.plot(self.L_array, self.fista_loss_train, label=fista_label, color=fista_color, marker=fista_marker, linewidth=linewidth, markersize=markersize)
+        plt.errorbar(self.L_array, self.fista_loss_train, yerr=3 * self.fista_loss_train_std, color=fista_color)
+        plt.plot(self.L_array, self.freq_loss_train, label=freq_label, color=freq_color, marker=freq_marker, linewidth=linewidth, markersize=markersize)
+        plt.errorbar(self.L_array, self.freq_loss_train, yerr=3 * self.freq_loss_train_std, color=freq_color)
+        plt.plot(self.L_array, self.sh_bayes_loss_train, label=sh_bayes_label, color=sh_bayes_color, marker=sh_bayes_marker, linewidth=linewidth, markersize=markersize)
+        plt.errorbar(self.L_array, self.sh_bayes_loss_train, yerr=3 * self.sh_bayes_loss_train_std, color=sh_bayes_color)
         plt.legend()
-        plt.title('NMSE train')
         plt.savefig('results/nmse_train.eps', format='eps')
+        plt.xlabel(r'K')
+        plt.ylabel(r'NMSE')
         plt.show()
 
-        plt.plot(self.L_array, self.ista_loss_validation, label='ista')
-        plt.plot(self.L_array, self.fista_loss_validation, label='fista')
-        plt.plot(self.L_array, self.freq_loss_validation, label='freq lista')
-        plt.plot(self.L_array, self.sh_bayes_loss_validation, label='sh bayes')
+        plt.plot(self.L_array, self.ista_loss_validation, label=ista_label, color=ista_color, marker=ista_marker, linewidth=linewidth, markersize=markersize)
+        plt.errorbar(self.L_array, self.ista_loss_validation, yerr=3 * self.ista_loss_validation_std, color=ista_color)
+        plt.plot(self.L_array, self.fista_loss_validation, label=fista_label, color=fista_color, marker=fista_marker, linewidth=linewidth, markersize=markersize)
+        plt.errorbar(self.L_array, self.fista_loss_validation, yerr=3 * self.fista_loss_validation_std, color=fista_color)
+        plt.plot(self.L_array, self.freq_loss_validation, label=freq_label, color=freq_color, marker=freq_marker, linewidth=linewidth, markersize=markersize)
+        plt.errorbar(self.L_array, self.freq_loss_validation, yerr=3 * self.freq_loss_validation_std, color=freq_color)
+        plt.plot(self.L_array, self.sh_bayes_loss_validation, label=sh_bayes_label, color=sh_bayes_color, marker=sh_bayes_marker, linewidth=linewidth, markersize=markersize)
+        plt.errorbar(self.L_array, self.sh_bayes_loss_validation, yerr=3 * self.sh_bayes_loss_validation_std, color=sh_bayes_color)
         plt.legend()
-        plt.title('NMSE validation')
         plt.savefig('results/nmse_validation.eps', format='eps')
+        plt.xlabel(r'K')
+        plt.ylabel(r'NMSE')
         plt.show()
 
 
